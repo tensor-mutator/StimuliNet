d98061d58a6b3e8d76c8bb88d0555db612beaada
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-07-13 04:02:21 +0530

    Update mutator.py

--- StimuliNet/FlowNet2/mutator.py
+++ StimuliNet/FlowNet2/mutator.py
@@ -54,7 +54,7 @@ class Mutator(object):
           return conv2d
 
       @staticmethod
-      def InterConv2D(filters: int, kernel_size: Tuple[int, int], strides: Tuple[int, int], batch_norm: bool = True, name: str = None) -> Callable:
+      def ConvInter2D(filters: int, kernel_size: Tuple[int, int], strides: Tuple[int, int], batch_norm: bool = True, name: str = None) -> Callable:
 	  if name:
 	     if batch_norm:
 		Mutator._set_name_to_instance(name, f'{name}/cond/Identity')
