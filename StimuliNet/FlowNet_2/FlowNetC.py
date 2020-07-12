9329662cb26ca0970f1e7075c21cb5dc6bd7aa81
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-07-12 19:29:17 +0530

    Update FlowNetC.py

--- StimuliNet/FlowNet_2/FlowNetC.py
+++ StimuliNet/FlowNet_2/FlowNetC.py
@@ -18,27 +18,27 @@ class FlowNetC(object):
           self._batch_norm = batch_norm
           self._div_flow = div_flow
 
-      def _fusion_stream(self) -> Callable:
+      def _fusion_stream(self, name: str) -> Callable:
 	  def create_fusion_stream(input: tf.Tensor) -> tf.Tensor
-	      conv1 = Mutator.Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), batch_norm=self._batch_norm)(input)
-	      conv2 = Mutator.Conv2D(filters=128, kernel_size=(5, 5), strides=(2, 2), batch_norm=self._batch_norm)(conv1)
-	      conv3 = Mutator.Conv2D(filters=256, kernel_size=(5, 5), strides=(2, 2), batch_norm=self._batch_norm)(conv2)
+	      conv1 = Mutator.Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), batch_norm=self._batch_norm, name=f'conv1{name}')(input)
+	      conv2 = Mutator.Conv2D(filters=128, kernel_size=(5, 5), strides=(2, 2), batch_norm=self._batch_norm, name=f'conv2{name}')(conv1)
+	      conv3 = Mutator.Conv2D(filters=256, kernel_size=(5, 5), strides=(2, 2), batch_norm=self._batch_norm, name=f'conv3{name}')(conv2)
 	      return conv3
           return create_fusion_stream
 
       def _build_model(self) -> tf.Tensor:
-	  stream1_tensor_out = self._fusion_stream()(self._image_1)
-	  stream2_tensor_out = self._fusion_stream()(self._image_2)
+	  stream1_tensor_out = self._fusion_stream()(self._image_1, 'a')
+	  stream2_tensor_out = self._fusion_stream()(self._image_2, 'b')
           corr_out = Correlation(pad_size=20, kernel_size=1, max_displacement=20, stride1=1, stride2=2, corr_multiply=1)(stream1_tensor_out, stream2_tensor_out)
 	  corr_out = layers.Activation(lambda x: tf.nn.leaky_relu(x, alpha=0.1))(corr_out)
           conv_redir_out = Mutator.Conv2D(filters=32, kernel_size=(1, 1), strides=(1, 1), batch_norm=self._batch_norm)(stream1_tensor_out)
-	  merged = tf.concat([conv_redir_out, corr_out], axis=1)
-          conv3_1 = Mutator.Conv2D(filters=256, kernel_size=(3, 3), batch_norm=self._batch_norm)(merged)
-          conv3_2 = Mutator.Conv2D(filters=512, kernel_size=(3, 3), strides=(2, 2), batch_norm=self._batch_norm)(conv3_1)
-          conv3_2_1 = Mutator.Conv2D(filters=512, kernel_size=(3, 3), batch_norm=self._batch_norm)(conv3_2)
-          conv3_3 = Mutator.Conv2D(filters=512, kernel_size=(3, 3), strides=(2, 2), batch_norm=self._batch_norm)(conv3_2_1)
-          conv3_3_1 = Mutator.Conv2D(filters=512, kernel_size=(3, 3), batch_norm=self._batch_norm)(conv3_3)
-          conv3_4 = Mutator.Conv2D(filters=1024, kernel_size=(3, 3), strides=(2, 2), batch_norm=self._batch_norm)(conv3_3_1)
-          conv3_4_1 = Mutator.Conv2D(filters=1024, kernel_size=(3, 3), batch_norm=self._batch_norm)(conv3_4)
-	  flow1 = Mutator.PredictFlow()(conv3_4_1)
-	  layers.ConvTranspose2D()
+	  fused = tf.concat([conv_redir_out, corr_out], axis=1)
+          conv3_1 = Mutator.Conv2D(filters=256, kernel_size=(3, 3), batch_norm=self._batch_norm, name='conv3_1')(fused)
+          conv4 = Mutator.Conv2D(filters=512, kernel_size=(3, 3), strides=(2, 2), batch_norm=self._batch_norm, name='conv4')(conv3_1)
+          conv4_1 = Mutator.Conv2D(filters=512, kernel_size=(3, 3), batch_norm=self._batch_norm, name='conv4_1')(conv4)
+          conv5 = Mutator.Conv2D(filters=512, kernel_size=(3, 3), strides=(2, 2), batch_norm=self._batch_norm, name='conv5')(conv4_1)
+          conv5_1 = Mutator.Conv2D(filters=512, kernel_size=(3, 3), batch_norm=self._batch_norm, name='conv5_1')(conv5)
+          conv6 = Mutator.Conv2D(filters=1024, kernel_size=(3, 3), strides=(2, 2), batch_norm=self._batch_norm, name='conv6')(conv5_1)
+          conv6_1 = Mutator.Conv2D(filters=1024, kernel_size=(3, 3), batch_norm=self._batch_norm, name='conv6_1')(conv6)
+	  flow6 = Mutator.PredictFlow()(conv6_1)
+	  flow6_up = layers.Conv2DTranspose()
