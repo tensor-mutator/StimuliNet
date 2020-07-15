2a8f8cdaaa1009ca1198ede29beb484a8e81c8cb
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-07-16 02:23:28 +0530

    Update __init__.py

--- StimuliNet/FlowNet2/__init__.py
+++ StimuliNet/FlowNet2/__init__.py
@@ -2,3 +2,4 @@ from .mutator import Mutator
 from .FlowNetC.flownet_c import FlowNetC
 from .FlowNetS.flownet_s import FlowNetS
 from .FlowNetFusion.flownet_fusion import FlowNetFusion
+from .FlowNetSD.flownet_sd import FlowNetSD
