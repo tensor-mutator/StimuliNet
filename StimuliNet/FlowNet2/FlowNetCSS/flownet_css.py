9390c4a2c2d75a0032add43860fad19719d193cb
tensor-mutator <draychaudhuri1086@gmail.com>
2020-07-21 02:03:25 +0530

    FlowNetCSS implemented

--- StimuliNet/FlowNet2/FlowNetCSS/flownet_css.py
+++ StimuliNet/FlowNet2/FlowNetCSS/flownet_css.py
@@ -1,11 +1,11 @@
 """
 @author: Debajoti Raychaudhuri
 
-A TensorFlow implementation of FlowNetCS
+A TensorFlow implementation of FlowNetCSS
 """
 
 from FlowNet2.network import Network
-from FlowNet2.FlowNetC import FlowNetC
+from FlowNet2.FlowNetC import FlowNetCS
 from FlowNet.FlowNetS import FlowNetS
 from FlowNet2.mutator import Mutator
 from FlowNet2.flow_warp import FlowWarp
@@ -18,7 +18,7 @@ class FlowNetCS(Network):
           self._image = image
           self._batch_norm = batch_norm
           self._div_flow = div_flow
-          self._scope = 'FlowNetCS'
+          self._scope = 'FlowNetCSS'
 
       def _build_graph_with_scope(self) -> tf.Graph:
           self.graph = tf.Graph()
@@ -33,15 +33,15 @@ class FlowNetCS(Network):
 
       def _build_graph(self) -> None:
           Mutator.set_graph(self.graph)
-          self._image_1 = tf.placeholder(shape=(None,) + image, dtype=tf.float32, name='image_1_cs')
-          self._image_2 = tf.placeholder(shape=(None,) + image, dtype=tf.float32, name='image_2_cs')
-          flownet_c = FlowNetC(self._image, self._batch_norm, self._div_flow)
-          flownet_c_patch = tf.import_graph_def(flownet_c.graph_def,
+          self._image_1 = tf.placeholder(shape=(None,) + image, dtype=tf.float32, name='image_1_css')
+          self._image_2 = tf.placeholder(shape=(None,) + image, dtype=tf.float32, name='image_2_css')
+          flownet_cs = FlowNetCS(self._image, self._batch_norm, self._div_flow)
+          flownet_cs_patch = tf.import_graph_def(flownet_cs.graph_def,
                                                 input_map={x.name: [self._image_1, self._image_2][i] for i, x in enumerate(flownet_c.inputs)},
                                                 return_elements=list(map(lambda x: x.name, flownet_c.outputs)))
-          flownet_s_input_tensor = self._compute_input_tensor_flow_flownet_s(self._image_1, self._image_2, flownet_c_patch)
+          flownet_s_input_tensor = self._compute_input_tensor_flow_flownet_s(self._image_1, self._image_2, flownet_cs_patch)
           flownet_s = FlowNetS(flownet_s_input_tensor.get_shape(), self._batch_norm)
-          self._flownet_cs_patch = tf.import_graph_def(flownet_s.graph_def,
+          self._flownet_css_patch = tf.import_graph_def(flownet_s.graph_def,
                               input_map={flownet_s.inputs[0].name: flownet_s_input_tensor},
                               return_elements=list(map(lambda x: x.name, flownet_s.outputs)))
 
@@ -56,7 +56,7 @@ class FlowNetCS(Network):
 
       @property
       def outputs(self) -> equence[tf.Tensor]:
-          return [self._flownet_cs_patch]
+          return [self._flownet_css_patch]
 
       def get_graph(self, dest: str = os.getcwd()) -> None:
           writer = tf.summary.FileWriter(dest, graph=self.graph)
--- StimuliNet/FlowNet2/__init__.py
+++ StimuliNet/FlowNet2/__init__.py
@@ -3,5 +3,6 @@ from .network import Network
 from .FlowNetC.flownet_c import FlowNetC
 from .FlowNetS.flownet_s import FlowNetS
 from .FlowNetCS.flownet_cs import FlowNetCS
+from .FlowNetCSS.flownet_css import FlowNetCSS
 from .FlowNetFusion.flownet_fusion import FlowNetFusion
 from .FlowNetSD.flownet_sd import FlowNetSD
