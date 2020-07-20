741b98ad4d3049fc324cc6db75898e075503b5b9
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-07-20 16:39:00 +0530

    Update flownet_fusion.py

--- StimuliNet/FlowNet2/FlowNetFusion/flownet_fusion.py
+++ StimuliNet/FlowNet2/FlowNetFusion/flownet_fusion.py
@@ -7,7 +7,7 @@ A TensorFlow implementation of FlowNetFusion
 from __future__ import print_function, division, absolute_import
 import tensorflow.compat.v1 as tf
 import tensorflow.compat.v1.keras.layers as layers
-from typing import Tuple, Callable
+from typing import Tuple, Callable, Sequence
 from FlowNet2.mutator import Mutator
 from FlowNet.network import Network
 import os
@@ -16,7 +16,7 @@ class FlowNetFusion(Network):
 
       def __init__(self, patch: Tuple[int, int, int], batch_norm: bool = True) -> None:
           self._batch_norm = batch_norm
-          self._input = tf.placeholder(dtype=tf.float32, shape=(None,) + patch, name='fusion_input')
+          self._patch = patch
           self._scope = 'FlowNetFusion'
           self._build_graph_with_scope()
 
@@ -29,6 +29,7 @@ class FlowNetFusion(Network):
 
       def _build_graph(self) -> None:
           Mutator.set_graph(self.graph)
+          self._input = tf.placeholder(dtype=tf.float32, shape=(None,) + self._patch, name='fusion_input')
           self._downsampling()
           self._upsampling()
 
@@ -50,7 +51,15 @@ class FlowNetFusion(Network):
           deconv0 = Mutator.Deconv(filters=16, name='deconv0')(fuse1)
           fuse0 = tf.concat([Mutator.get_operation(self._names.get('conv0')), deconv0, flow1_up], axis=1, name='fuse0')
           interconv0 = Mutator.Conv2DInter(filters=16, kernel_size=(3, 3), batch_norm=self._batch_norm, name='interconv0')(fuse0)
-          flow0 = Mutator.PredictFlow(name='flow0')(interconv0)
+          self._flow0 = Mutator.PredictFlow(name='flow0')(interconv0)
+
+      @property
+      def inputs(self) -> Sequence[tf.Tensor]:
+          return [self._input]
+
+      @property
+      def outputs(self) -> Sequence[tf.Tensor]:
+          return [self._flow0]
 
       def get_graph(self, dest: str = os.getcwd()) -> None:
           writer = tf.summary.FileWriter(dest, graph=self.graph)
