e783cdab0544139d4d9b2b620642952422c33d55
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-07-25 02:24:04 +0530

    Update network.py

--- StimuliNet/FlowNet2/network.py
+++ StimuliNet/FlowNet2/network.py
@@ -29,7 +29,3 @@ class Network(metaclass=ABCMeta):
       @abstractmethod
       def get_graph(self, dest: str) -> None:
           ...
-
-      @abstractmethod
-      def loss(self, flow: tf.Tensor, predictions: tf.Tensor) -> tf.Tensor:
-          ...
