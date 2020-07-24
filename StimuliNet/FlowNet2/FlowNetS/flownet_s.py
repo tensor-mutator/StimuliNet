46fa4a46ff9a7e98a06a2d8781091fcca292d60c
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-07-25 02:23:25 +0530

    Update flownet_s.py

--- StimuliNet/FlowNet2/FlowNetS/flownet_s.py
+++ StimuliNet/FlowNet2/FlowNetS/flownet_s.py
@@ -27,6 +27,8 @@ class FlowNetS(Network):
           with self.graph.as_default():
                with tf.variable_scope(self._scope):
                     self._build_graph()
+                    loss_input_output = self._build_loss_ops()
+                    self.loss = type('loss', (object,), loss_input_output)
           return self.graph
 
       def _build_graph(self) -> None:
@@ -83,8 +85,8 @@ class FlowNetS(Network):
           writer = tf.summary.FileWriter(dest, graph=self.graph)
           writer.close()
 
-      def loss(self, flow: np.ndarray, predictions: tf.Tensor) -> tf.Tensor:
-          flow = tf.convert_to_tensor(flow, dtype=tf.float32)
+      def _build_loss_ops(self) -> tf.Tensor:
+          flow = tf.placeholder(dtype=tf.float32, shape=(None,) + self._image)
           flow = flow * 0.05
           losses = list()
           flow6 = Mutator.get_operation(self._names.get('flow6'))
@@ -103,4 +105,4 @@ class FlowNetS(Network):
           flow2_labels = Downsample(flow, [flow2.shape[1], flow2.shape[2]])
           losses.append(Mutator.average_endpoint_error(flow2_labels, flow2))
           loss = tf.losses.compute_weighted_loss(losses, [0.32, 0.08, 0.02, 0.01, 0.005])
-          return tf.losses.get_total_loss()
+          return dict(input=flow, output=tf.losses.get_total_loss())
