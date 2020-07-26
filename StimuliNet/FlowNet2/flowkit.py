96ec418a70e45ebbcf86adccf9b59cd89e2c6c16
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-07-26 22:20:27 +0530

    Update flowkit.py

--- StimuliNet/FlowNet2/flowkit.py
+++ StimuliNet/FlowNet2/flowkit.py
@@ -1,5 +1,31 @@
 import numpy as np
 
+def compute_flow_color(x, y):
+    [h, w] = x.shape
+    img = np.zeros([h, w, 3])
+    nanIdx = np.isnan(x) | np.isnan(y)
+    x[nanIdx] = 0
+    y[nanIdx] = 0
+    ncols = np.size(color_palette, 0)
+    rad = np.sqrt(x**2+y**2)
+    a = np.arctan2(-y, -x) / np.pi
+    fk = (a+1) / 2 * (ncols - 1) + 1
+    k0 = np.floor(fk).astype(int)
+    k1 = k0 + 1
+    k1[k1 == ncols+1] = 1
+    f = fk - k0
+    for i in range(0, np.size(color_palette,1)):
+        tmp = color_palette[:, i]
+        col0 = tmp[k0-1] / 255
+        col1 = tmp[k1-1] / 255
+        col = (1-f) * col0 + f * col1
+        idx = rad <= 1
+        col[idx] = 1-rad[idx]*(1-col[idx])
+        notidx = np.logical_not(idx)
+        col[notidx] *= 0.75
+        img[:, :, i] = np.uint8(np.floor(255 * col*(1-nanIdx)))
+    return img
+
 @property
 def color_palette():
     RY = 15
