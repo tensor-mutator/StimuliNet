d9518831597e444f049c4fe6c5f5ca2174ca7331
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-14 04:53:32 +0530

    Update network.py

--- StimuliNet/flownet2/flownet2/network.py
+++ StimuliNet/flownet2/flownet2/network.py
@@ -31,11 +31,15 @@ class Network(metaclass=ABCMeta):
       def get_graph(self, dest: str) -> None:
           ...
 
-      def model(self, X: tf.tensor, y: tf.Tensor) -> tf.Tensor:
-          return_ops = tf.import_graph_def(self.graph_def,
-                                           input_map: {self.inputs[0].name: X[:, 0, :, :, :],
-                                                       self.inputs[1].name: X[:, 1, :, :, :],
-                                                       self.loss.input.name: y},
-                                           return_elements: [self.loss.output])
-          self.cost = return_ops[0]
-          return self.cost
+      def model(self, X: tf.tensor, y: tf.Tensor = None) -> None:
+          if y is None:
+               self.y = tf.import_graph_def(self.graph_def,
+                                                input_map: {self.inputs[0].name: X[:, 0, :, :, :],
+                                                            self.inputs[1].name: X[:, 1, :, :, :]},
+                                                return_elements: [self.outputs[0].name])
+               return
+          self.cost, self.y = tf.import_graph_def(self.graph_def,
+                                                  input_map: {self.inputs[0].name: X[:, 0, :, :, :],
+                                                              self.inputs[1].name: X[:, 1, :, :, :],
+                                                              self.loss.input.name: y},
+                                                  return_elements: [self.loss.output.name, self.outputs[0].name])
