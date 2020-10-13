5bbe75c1d83fc5779230e8bb9f6dff0897052b27
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-13 05:43:55 +0530

    Update flownet_css.py

--- StimuliNet/flownet2/flownet2/FlowNetCSS/flownet_css.py
+++ StimuliNet/flownet2/flownet2/FlowNetCSS/flownet_css.py
@@ -16,11 +16,13 @@ from ..mutator import Mutator
 
 class FlowNetCSS(Network):
 
-      def __init__(self, image: Tuple[int, int, int], batch_norm: bool = True, trainable: bool = True) -> None:
+      def __init__(self, image: Tuple[int, int], flow: Tuple[int, int], batch_norm: bool = True, trainable: bool = True) -> None:
           self._image = image
+          self._flow = flow
           self._batch_norm = batch_norm
           self._trainable = trainable
           self._scope = 'FlowNetCSS'
+          self._build_graph_with_scope()
 
       def _build_graph_with_scope(self) -> tf.Graph:
           self.graph = tf.Graph()
@@ -37,28 +39,31 @@ class FlowNetCSS(Network):
 
       def _build_graph(self) -> None:
           Mutator.set_graph(self.graph)
-          self._image_1 = tf.placeholder(shape=(None,) + self._image, dtype=tf.float32, name='image_1_css')
-          self._image_2 = tf.placeholder(shape=(None,) + self._image, dtype=tf.float32, name='image_2_css')
+          self._image_1 = tf.placeholder(shape=(None,) + self._image + (3,), dtype=tf.float32, name='image_1_css')
+          self._image_2 = tf.placeholder(shape=(None,) + self._image + (3,), dtype=tf.float32, name='image_2_css')
           flownet_cs = FlowNetCS(self._image, self._batch_norm, trainable=False)
           flownet_cs_patch = tf.import_graph_def(flownet_cs.graph_def,
                                                  input_map={x.name: [self._image_1, self._image_2][i] for i, x in enumerate(flownet_cs.inputs)},
-                                                 return_elements=list(map(lambda x: x.name, flownet_cs.outputs)))
-          flownet_s_input_tensor = self._compute_input_tensor_for_flownet_s(self._image_1, self._image_2, flownet_cs_patch)
-          flownet_s = FlowNetS(flownet_s_input_tensor.get_shape(), self._batch_norm, trainable=self._trainable)
+                                                 return_elements=list(map(lambda x: x.name, flownet_cs.outputs)), name="FlowNetCS Graph")
+          flownet_s_input_tensor = self._compute_input_tensor_for_flownet_s(self._image_1, self._image_2, flownet_cs_patch[0])
+          flownet_s = FlowNetS(self._image, self._flow, self._batch_norm, trainable=self._trainable)
           if self._trainable:
-             self._flow_label = tf.placeholder(dtype=tf.float32, shape=(None,) + self._image)
+             self._flow_label = tf.placeholder(dtype=tf.float32, shape=(None,) + self._flow + (2,))
              self._flownet_cs_patch, self._loss = tf.import_graph_def(flownet_s.graph_def,
-                                                          input_map={flownet_s.inputs[0].name: flownet_s_input_tensor,
-                                                                     flownet_s.loss.input: self._flow_label},
-                                                          return_elements=list(map(lambda x: x.name, flownet_s.outputs)) + [flownet_s.loss.output])
+                                                                      input_map={flownet_s.inputs[0].name: flownet_s_input_tensor,
+                                                                                 flownet_s.loss.input.name: self._flow_label},
+                                                                      return_elements=list(map(lambda x: x.name,
+                                                                                               flownet_s.outputs)) + [flownet_s.loss.output.name],
+                                                                      name="FlowNetS Graph")
           else:
              self._flownet_css_patch = tf.import_graph_def(flownet_s.graph_def,
                                                            input_map={flownet_s.inputs[0].name: flownet_s_input_tensor},
-                                                           return_elements=list(map(lambda x: x.name, flownet_s.outputs)))
+                                                           return_elements=list(map(lambda x: x.name, flownet_s.outputs)),
+                                                           name="FlowNetS Graph")
 
       def _compute_input_tensor_for_flownet_s(self, image_1: tf.Tensor, image_2: tf.Tensor, flow_out: tf.Tensor) -> tf.Tensor:
           warped = dense_image_warp(image_2, flow_out)
-          brightness_error = Mutator.ChannelNorm()(image_1 - warped)
+          brightness_error = Mutator.layers.ChannelNorm()(image_1 - warped)
           return tf.concat([image_1, image_2, warped, flow_out * 0.05, brightness_error], axis=-1)
 
       @property
