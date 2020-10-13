5d3ed04e4bc48bc313c16132a0cf42ff93957e9a
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-14 03:04:39 +0530

    Update exceptions.py

--- StimuliNet/flownet2/flownet2/exceptions.py
+++ StimuliNet/flownet2/flownet2/exceptions.py
@@ -2,3 +2,8 @@ class NotTrainableError(Exception):
 
       def __init__(self, msg: str) -> None:
           super(NotTrainableError, self).__init__(msg)
+
+class InvalidHyperparameterError(Exception):
+
+      def __init__(self, msg: str) -> None:
+          super(InvalidHyperparameterError, self).__init__(msg)
