e6aef2ae17746df4d4d7a7aa2bc6fc8549c74508
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-16 02:29:02 +0530

    Update network.py

--- StimuliNet/flownet2/flownet2/network.py
+++ StimuliNet/flownet2/flownet2/network.py
@@ -32,15 +32,9 @@ class Network(metaclass=ABCMeta):
           ...
 
       def model(self, X: tf.Tensor, y: tf.Tensor = None) -> None:
-          if y is None:
-               self.y = tf.import_graph_def(self.graph_def,
-                                            input_map={self.inputs[0].name: X[:, 0, :, :, :],
-                                                       self.inputs[1].name: X[:, 1, :, :, :]},
-                                            return_elements=[self.outputs[0].name])
-               return
-          self.cost, self.y = tf.import_graph_def(self.graph_def,
-                                                  input_map={self.inputs[0].name: X[:, 0, :, :, :],
-                                                             self.inputs[1].name: X[:, 1, :, :, :],
-                                                             self.loss.input.name: y},
-                                                  return_elements=[self.loss.output.name, self.outputs[0].name])
-          self.src_img, self.dest_img = self.inputs
+          self.src_img, self.dest_img = self.inputs[0], self.inputs[1]
+          self.src_img, self.dest_img = X[:, 0, :, :, :], X[:, 1, :, :, :]
+          self.y = self.outputs[0]
+          if y:
+             self.loss.input = y
+             self.cost = self.loss.output
