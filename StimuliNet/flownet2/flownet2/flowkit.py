8e17fc2d4e52ff3469647392b00d9c24376b893a
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-16 05:16:41 +0530

    Update flowkit.py

--- StimuliNet/flownet2/flownet2/flowkit.py
+++ StimuliNet/flownet2/flownet2/flowkit.py
@@ -52,7 +52,7 @@ def read_flow(filename):
         w = np.fromfile(f, np.int32, count=1)
         h = np.fromfile(f, np.int32, count=1)
         print("Reading %d x %d flo file" % (h, w))
-        data2d = np.fromfile(f, np.float32, count=2 * w * h)
+        data2d = np.fromfile(f, np.float32, count=2 * w[0] * h[0])
         data2d = np.resize(data2d, (h[0], w[0], 2))
     f.close()
     return data2d
