3fb318282c4ae629982241a87469c4699bc61e34
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-17 02:57:28 +0530

    Update __main__.py

--- StimuliNet/flownet2/flownet2/FlowNetC/__main__.py
+++ StimuliNet/flownet2/flownet2/FlowNetC/__main__.py
@@ -12,8 +12,8 @@ weights_path = os.path.split(__file__)[0]
 
 def parser() -> ap:
     parser = ap(add_help=True)
-    parser.add_argument("-graph", "--get-graph", help="dumps an event file with the current graph", required=False, action="store_true")
-    parser.add_argument("-train", "--train", help="starts training the current block with the designated resolution", required=False)
+    parser.add_argument("-graph", "--get-graph", help="dumps an event file with the FlowNetC graph", required=False, action="store_true")
+    parser.add_argument("-train", "--train", help="starts training the FlowNetC block with the designated resolution", required=False)
     return parser
 
 def main(args: Namespace):
