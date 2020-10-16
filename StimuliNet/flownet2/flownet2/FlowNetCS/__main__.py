ee5b044f0c66dc9ac54d1715a03bb7a18ee7123a
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-17 03:04:14 +0530

    Update __main__.py

--- StimuliNet/flownet2/flownet2/FlowNetCS/__main__.py
+++ StimuliNet/flownet2/flownet2/FlowNetCS/__main__.py
@@ -5,6 +5,7 @@ import sys
 import os
 import re
 from .flownet_cs import FlowNetCS
+from ..FlowNetC import flownet_c
 from ..pipeline import Pipeline
 from ..config import config
 
@@ -18,8 +19,10 @@ def parser() -> ap:
 
 def main(args: Namespace):
     if args.train:
+       frozen_config = [dict(scope="FlowNetC", path=os.path.split(flownet_c.__file__)[0])]
        resolution = tuple(list(map(lambda x: int(x), re.findall(r'[0-9]{1,}', args.train))))
-       pipeline = Pipeline(FlowNetCS, "DEFAULT", resolution, resolution, checkpoint_path=weights_path, config=config.LOSS_EVENT+config.SAVE_FLOW)
+       pipeline = Pipeline(FlowNetCS, "DEFAULT", resolution, resolution, checkpoint_path=weights_path, frozen_config=frozen_config,
+                           config=config.LOSS_EVENT+config.SAVE_FLOW)
        X_src_train, X_src_test, X_dest_train, X_dest_test, y_train, y_test = load(resolution, resolution)
        pipeline.fit(X_src_train, X_src_test, X_dest_train, X_dest_test, y_train, y_test)
     if args.get_graph:
