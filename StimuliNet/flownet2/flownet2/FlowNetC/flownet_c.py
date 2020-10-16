4efb185d492a56ce5742082def43089af5f8ca24
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-17 01:57:47 +0530

    Update flownet_c.py

--- StimuliNet/flownet2/flownet2/FlowNetC/flownet_c.py
+++ StimuliNet/flownet2/flownet2/FlowNetC/flownet_c.py
@@ -89,7 +89,7 @@ class FlowNetC(Network):
           flow3_up = Mutator.layers.Upconv(name='flow3_up')(flow3)
           deconv2 = Mutator.layers.Deconv(filters=64, name='deconv2')(fuse3)
           fuse2 = tf.concat([Mutator.get_operation(self._names.get('conv2a')), deconv2, flow3_up], axis=3, name='fuse2')
-          flow2 = Mutator.layers.Conv2DFlow(name='flow2', scale=20.0, resize=self._image,
+          flow2 = Mutator.layers.Conv2DFlow(name='flow2', scale=20.0, resize=(tf.shape(self._image_src)[1], tf.shape(self._image_src)[2])
                                             kernel_regularizer=tf.keras.regularizers.l2(self._l2))(fuse2)
 
       @property
