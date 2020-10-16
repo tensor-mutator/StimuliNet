7bd622d40ad40f1845e3b09964c485bf2186c7df
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-17 01:06:32 +0530

    Update __main__.py

--- StimuliNet/flownet2/flownet2/FlowNetC/__main__.py
+++ StimuliNet/flownet2/flownet2/FlowNetC/__main__.py
@@ -1,11 +1,24 @@
+from argparse import ArgumentParser as ap
+from argparse import NameSpace
+from flow_dataset.mpi_sintel import load
+import sys
 from .flownet_c import FlowNetC
 from ..pipeline import Pipeline
 from ..config import config
-from flow_dataset.mpi_sintel import load
 
-def main():
-    pipeline = Pipeline(FlowNetC, "DEFAULT", (512, 512), (512, 512), config=config.LOSS_EVENT+config.SAVE_FLOW)
-    X_train, X_test, y_train, y_test = load((512, 512), (512, 512))
-    pipeline.fit(X_train, X_test, y_train, y_test)
+def parser() -> ap:
+    parser = ap(add_help=True)
+    parser.add_argument("-graph", "--get-graph", help="dumps an event file with the current graph", required=False)
+    parser.add_argument("-train", "--train", help="starts training the current block", required=False, action="store_true")
+    return parser
 
-main()
+def main(args: NameSpace):
+    if args.train:
+       pipeline = Pipeline(FlowNetC, "DEFAULT", (512, 512), (512, 512), config=config.LOSS_EVENT+config.SAVE_FLOW)
+       X_train, X_test, y_train, y_test = load((512, 512), (512, 512))
+       pipeline.fit(X_train, X_test, y_train, y_test)
+    if args.get_graph:
+       pipeline = Pipeline(FlowNetC, "DEFAULT", (512, 512), (512, 512), config=config.LOSS_EVENT+config.SAVE_FLOW)
+       pipeline.save_graph()
+
+main(parser().parse_args(sys.argv[1:]))
