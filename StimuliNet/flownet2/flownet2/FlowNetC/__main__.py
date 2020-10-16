1ccb86d785b62aae767119423096a12391eb2874
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-17 01:09:08 +0530

    Update __main__.py

--- StimuliNet/flownet2/flownet2/FlowNetC/__main__.py
+++ StimuliNet/flownet2/flownet2/FlowNetC/__main__.py
@@ -2,9 +2,12 @@ from argparse import ArgumentParser as ap
 from argparse import NameSpace
 from flow_dataset.mpi_sintel import load
 import sys
+import os
 from .flownet_c import FlowNetC
 from ..pipeline import Pipeline
 from ..config import config
+
+weights_path = os.path.split(__file__)[0]
 
 def parser() -> ap:
     parser = ap(add_help=True)
@@ -14,11 +17,11 @@ def parser() -> ap:
 
 def main(args: NameSpace):
     if args.train:
-       pipeline = Pipeline(FlowNetC, "DEFAULT", (512, 512), (512, 512), config=config.LOSS_EVENT+config.SAVE_FLOW)
+       pipeline = Pipeline(FlowNetC, "DEFAULT", (512, 512), (512, 512), path=weights_path, config=config.LOSS_EVENT+config.SAVE_FLOW)
        X_train, X_test, y_train, y_test = load((512, 512), (512, 512))
        pipeline.fit(X_train, X_test, y_train, y_test)
     if args.get_graph:
-       pipeline = Pipeline(FlowNetC, "DEFAULT", (512, 512), (512, 512), config=config.LOSS_EVENT+config.SAVE_FLOW)
+       pipeline = Pipeline(FlowNetC, "DEFAULT", (512, 512), (512, 512), path-weights_path, config=config.LOSS_EVENT+config.SAVE_FLOW)
        pipeline.save_graph()
 
 main(parser().parse_args(sys.argv[1:]))
