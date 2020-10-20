0b10022f3647776ceefc88a92ffd866a8856193e
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-20 10:25:35 +0530

    Update __main__.py

--- StimuliNet/flownet2/flownet2/FlowNetSD/__main__.py
+++ StimuliNet/flownet2/flownet2/FlowNetSD/__main__.py
@@ -19,11 +19,11 @@ def parser() -> ap:
 def main(args: Namespace) -> None:
     if args.train:
        resolution = tuple(list(map(lambda x: int(x), re.findall(r'[0-9]{1,}', args.train))))
-       pipeline = Pipeline(FlowNetSD, "DEFAULT", resolution, resolution, checkpoint_path=weights_path, config=config.LOSS_EVENT+config.SAVE_FLOW)
+       pipeline = Pipeline(FlowNetSD, "LONG_SCHEDULE", resolution, resolution, checkpoint_path=weights_path, config=config.LOSS_EVENT+config.SAVE_FLOW)
        X_src_train, X_src_test, X_dest_train, X_dest_test, y_train, y_test = load(resolution, resolution)
        pipeline.fit(X_src_train, X_src_test, X_dest_train, X_dest_test, y_train, y_test)
     if args.get_graph:
-       pipeline = Pipeline(FlowNetSD, "DEFAULT", (512, 512), (512, 512), checkpoint_path=weights_path, config=config.LOSS_EVENT+config.SAVE_FLOW)
+       pipeline = Pipeline(FlowNetSD, "LONG_SCHEDULE", (512, 512), (512, 512), checkpoint_path=weights_path, config=config.LOSS_EVENT+config.SAVE_FLOW)
        pipeline.save_graph(weights_path)
 
 main(parser().parse_args(sys.argv[1:]))
