9d8f6c2186c9727e80ea74a2a12536e74bf72aa1
Debajyoti Raychaudhuri <draychaudhuri1086@gmail.com>
2020-10-20 10:24:41 +0530

    Update __main__.py

--- StimuliNet/flownet2/flownet2/FlowNetCS/__main__.py
+++ StimuliNet/flownet2/flownet2/FlowNetCS/__main__.py
@@ -26,7 +26,7 @@ def main(args: Namespace) -> None:
        X_src_train, X_src_test, X_dest_train, X_dest_test, y_train, y_test = load(resolution, resolution)
        pipeline.fit(X_src_train, X_src_test, X_dest_train, X_dest_test, y_train, y_test)
     if args.get_graph:
-       pipeline = Pipeline(FlowNetCS, "DEFAULT", (512, 512), (512, 512), checkpoint_path=weights_path, config=config.LOSS_EVENT+config.SAVE_FLOW)
+       pipeline = Pipeline(FlowNetCS, "LONG_SCHEDULE", (512, 512), (512, 512), checkpoint_path=weights_path, config=config.LOSS_EVENT+config.SAVE_FLOW)
        pipeline.save_graph(weights_path)
 
 main(parser().parse_args(sys.argv[1:]))
